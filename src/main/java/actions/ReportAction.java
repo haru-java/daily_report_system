af3a42883ecae2634f9a129ec8fde83b340a393f
haruka.iwabu <haruka.iwabu@techacademy.jp>
2023-07-31 19:59:31 +0900

    日報indexアクションの作成

---
+++ src/main/java/actions/ReportAction.java
@@ -0,0 +1,76 @@
+/*
+Lesson 17Chapter 9.6
+Reportに対するアクションとビューの作成1：indexの作成その１
+
+(Report の7つのアクションである index、new、create、show、edit、update を作成。)
+のなかのここはindexさんのアクション。
+Report と Employee というリソースの違いはありますが、実施している内容は EmployeeAction の index() メソッドとと同じ
+続きは
+/src/main/webapp/WEB-INF/views/reports/index.jspのビューへ。
+ */
+
+package actions;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import actions.views.ReportView;
+import constants.AttributeConst;
+import constants.ForwardConst;
+import constants.JpaConst;
+import services.ReportService;
+
+/**
+ * 日報に関する処理を行うActionクラス
+ *
+ */
+public class ReportAction extends ActionBase {
+
+    private ReportService service;
+
+    /**
+     * メソッドを実行する
+     */
+    @Override
+    public void process() throws ServletException, IOException {
+
+        service = new ReportService();
+
+        //メソッドを実行
+        invoke();
+        service.close();
+    }
+
+    /**
+     * 一覧画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void index() throws ServletException, IOException {
+
+        //指定されたページ数の一覧画面に表示する日報データを取得
+        int page = getPage();
+        List<ReportView> reports = service.getAllPerPage(page);
+
+        //全日報データの件数を取得
+        long reportsCount = service.countAll();
+
+        putRequestScope(AttributeConst.REPORTS, reports); //取得した日報データ
+        putRequestScope(AttributeConst.REP_COUNT, reportsCount); //全ての日報データの件数
+        putRequestScope(AttributeConst.PAGE, page); //ページ数
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //1ページに表示するレコードの数
+
+        //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if (flush != null) {
+            putRequestScope(AttributeConst.FLUSH, flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        //一覧画面を表示
+        forward(ForwardConst.FW_REP_INDEX);
+    }
+
+}
