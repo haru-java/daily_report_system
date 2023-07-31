707348a7500ca71952f06927447c2537c08cb073
haruka.iwabu <haruka.iwabu@techacademy.jp>
2023-07-31 18:07:20 +0900

    ログイン画面表示アクションの作成

---
+++ src/main/java/actions/AuthAction.java
@@ -0,0 +1,62 @@
+/*
+Lesson 17Chapter 8.1
+ログインの仕組みその１
+ログイン画面を表示する機能を作成しよう
+続きは、/src/main/webapp/WEB-INF/views/login/login.jspへ。
+
+ */
+
+package actions;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import constants.AttributeConst;
+import constants.ForwardConst;
+import services.EmployeeService;
+
+/**
+ * 認証に関する処理を行うActionクラス
+ *
+ */
+public class AuthAction extends ActionBase {
+
+    private EmployeeService service;
+
+    /**
+     * メソッドを実行する
+     */
+    @Override
+    public void process() throws ServletException, IOException {
+
+        service = new EmployeeService();
+
+        //メソッドを実行
+        invoke();
+
+        service.close();
+    }
+
+    /**
+     * ログイン画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void showLogin() throws ServletException, IOException {
+
+        //CSRF対策用トークンを設定
+        putRequestScope(AttributeConst.TOKEN, getTokenId());
+
+        //セッションにフラッシュメッセージが登録されている場合はリクエストスコープに設定する
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if (flush != null) {
+            putRequestScope(AttributeConst.FLUSH,flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        //ログイン画面を表示
+        forward(ForwardConst.FW_LOGIN);
+    }
+
+}
